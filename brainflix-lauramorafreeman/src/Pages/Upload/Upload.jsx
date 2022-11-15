e0456876178408c3b230dee2abf4a92759f85d6d
laurisfree <freelau00@gmail.com>
2022-11-14 23:53:06 -0800

    upload styling

--- brainflix-lauramorafreeman/src/Pages/Upload/Upload.jsx
+++ brainflix-lauramorafreeman/src/Pages/Upload/Upload.jsx
@@ -47,18 +47,21 @@ function VideoPlayer (){
     return (
             <form className="video-play" onSubmit={handleSubmit}>
                 <h1 className="video-play__title">Upload Video</h1>
-                <h3 className="video-play__label-thumb">VIDEO THUMBNAIL</h3>
-                <img className="video-play__holder" src={videoPreview} alt="" />
-                <label className="video-play__label-your-video">TITLE YOUR VIDEO</label>
-                <input name="title" className="video-play__label-your-video-input" type="text" placeholder="Add a title to your video" onChange={handleTitle}/>
-                <label className="video-play__label-description">ADD A VIDEO DESCRIPTION</label>
-                <input name="description" className="video-play__label-your-video-description-input" type="text" placeholder="Add a description to your video" onChange={handleDescription}/>
-                <div className='video-play__btn-wrapper'>
-                    
-                    <button className="video-play__publish-btn" src={arrow}>PUBLISH
-                    </button>
-                
-                <button className="video-play__cancel-btn">CANCEL</button>
+                <div className='video-play__main-wrapper'>
+                    <div className="video-play__wrapper-1">
+                        <h3 className="video-play__label-thumb">VIDEO THUMBNAIL</h3>
+                        <img className="video-play__holder" src={videoPreview} alt="" />
+                    </div>
+                    <div className="video-play__wrapper-2">
+                        <label className="video-play__label-your-video">TITLE YOUR VIDEO</label>
+                        <input name="title" className="video-play__label-your-video-input" type="text" placeholder="Add a title to your video" onChange={handleTitle}/>
+                        <label className="video-play__label-description">ADD A VIDEO DESCRIPTION</label>
+                        <input name="description" className="video-play__label-your-video-description-input" type="text" placeholder="Add a description to your video" onChange={handleDescription}/>
+                        <div className='video-play__btn-wrapper'>  
+                            <button className="video-play__publish-btn" src={arrow}>PUBLISH</button>
+                            <button className="video-play__cancel-btn">CANCEL</button>
+                        </div>
+                    </div>
                 </div>
             </form>
     );
