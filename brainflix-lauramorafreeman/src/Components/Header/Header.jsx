4a59caa453091374644b18a7d03d4cf1fd6810f9
laurisfree <freelau00@gmail.com>
2022-11-20 21:25:37 -0800

    header

--- brainflix-lauramorafreeman/src/Components/Header/Header.jsx
+++ brainflix-lauramorafreeman/src/Components/Header/Header.jsx
@@ -13,16 +13,28 @@ function Header () {
                 <img className="header__image" src={logo} alt="brainflix-logo" />
                 </Link>
             </div>
-            <div className='header__avatar-wrapper flex-item'>
-                <img className="header__avatar-1" src={image} alt="brainflix-avatar"/>
-            </div>
-            <div className="header__search-wrapper" class="flex-item flex-item">
+            <div className="header__search-wrapper-2" class="flex-item flex-item">
+                    <input className="header__search-2" type="text" placeholder = "      Search" />
+                </div>
+                <div className='header__avatar-wrapper-2 flex-item'>
+                    <img className="header__avatar-3" src={image} alt="brainflix-avatar"/>
+                </div>
+            <div className="header__search-avatar-wrapper" >
+                <div className="header__search-wrapper" class="flex-item flex-item">
+                    <input className="header__search" type="text" placeholder = "      Search" />
+                </div>
+                <div className='header__avatar-wrapper flex-item'>
+                    <img className="header__avatar-1" src={image} alt="brainflix-avatar"/>
+                </div>
+            {/* <div className="header__search-wrapper" class="flex-item flex-item">
                 <input className="header__search" type="text" placeholder = "      Search" />
+            </div> */}
             </div>
             <Link to="/upload" className='header__btn-wrapper' class="flex-item flex-item">
                 <button className="header__btn-up" type="file" accept="image/png, image/jpeg">UPLOAD</button>
             </Link>
         </div>
-    </div>);
+    </div>
+    );
 }
     export default Header
